13e9830b64ee0ccdc133c9e84b27c5cec07e75a0
jhalayashraj <jhalayashraj@gmail.com>
2021-10-09 04:13:33 +0530

    Added scope for report and searching

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,14 @@ class Customer < ApplicationRecord
   ## Validations
   validates :name, :email, presence: true
   validates :email, uniqueness: true
+
+  ## Scopes
+  scope :search_by_name_or_model, lambda { |term|
+                                    eager_load(:vehicles).where('name ILIKE :search OR model ILIKE :search',
+                                                                search: "%#{term}%")
+                                  }
+  scope :customers_by_nationality, -> { group(:nationality).count }
+  scope :average_odometer_reading_by_nationality, lambda {
+                                                    eager_load(:vehicles).group(:nationality).average(:odometer_reading)
+                                                  }
 end
